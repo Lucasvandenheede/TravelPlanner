d7090b20486c1099b75a249dddc8db82c3beb56c
Mathijs Ooms <r0929441@student.thomasmore.be>
2024-12-18 22:22:31 +0100

    Place en Travel

---
+++ place-service/src/main/java/fact/it/placeservice/dto/PlaceRequest.java
@@ -0,0 +1,20 @@
+package fact.it.placeservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PlaceRequest {
+    private String name;
+    private String description;
+    private String address;
+    private double rating;
+    private String image;
+    private String type;
+    private String status;
+}
---
+++ travel-service/src/main/java/fact/it/travelservice/dto/TravelResponse.java
@@ -0,0 +1,21 @@
+package fact.it.travelservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TravelResponse {
+    private Long id;
+    private String startLocation;
+    private String endLocation;
+    private String transportType;
+    private double price;
+    private String duration;
+    private String departureTime;
+    private String arrivalTime;
+}
